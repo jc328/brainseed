f0789a3c94e88482d8561bab7c756bf99e487d7e
John Chen <johnchen328@gmail.com>
2020-09-15 14:49:28 -0700

    added more public resources, links

--- client/src/components/LandingFooter.js
+++ client/src/components/LandingFooter.js
@@ -2,22 +2,31 @@ import React from 'react';
 import { Button } from 'antd';
 import 'antd/dist/antd.css';
 import '../styles/landingfooter.css'
-// import { LinkedinOutlined } from '@ant-design/icons';
+import { GithubOutlined } from '@ant-design/icons';
 
 function LandingFooter() {
     return (
         <>
           <div className="landingfooter_container">
-            <div>JOHN CHEN</div>
+            <div className="landingfooter_name">JOHN CHEN</div>
                 <div>
                     <Button type="link" href="https://www.linkedin.com/in/john-chen-92714817/"><img
                     src={process.env.PUBLIC_URL + '/linkedin.png'}
                     alt=""
-                    style={{height:20}}
+                    style={{height:20, marginRight: 20, marginLeft: 20}}
                      /></Button>
                 </div>
-            <div>Github</div>
-            <div>LinkedIn</div>
+            <div>
+                <Button type="link"
+                 icon={<GithubOutlined />}
+                 href="https://github.com/jc328"
+                 style={{color: 'white'}}
+                 >
+                     <img
+                    src={process.env.PUBLIC_URL + '/github.png'}
+                    alt=""
+                    style={{height:25}}
+                     /></Button></div>
             </div>
         </>
     );
